f8fad23b93d08ca559afd1368d6cac61c1b4d62c
JesusPerezLaparra <jesusp345@gmail.com>
2025-03-05 10:40:45 +0100

    Muestra info planeta cuando clica en personaje

--- script.js
+++ script.js
@@ -3,23 +3,7 @@ fetch("https://dragonball-api.com/api/characters?&limit=10")
     .then((data) => {
         botonpaginaAnt.setAttribute("href", data.links.previous);
         botonpaginaSig.setAttribute("href", data.links.next);
-        data.items.forEach((ch) => {
-            const nombrePersonaje = ch.name;
-            const energiaPersonaje = ch.ki;
-            const razaPersonaje = ch.race;
-            const generoPersonaje = ch.gender;
-            const imagenPersonaje = ch.image;
-            const descripcionPersonaje = ch.description;
-
-            nuevoPersonaje(
-                nombrePersonaje,
-                energiaPersonaje,
-                razaPersonaje,
-                generoPersonaje,
-                imagenPersonaje,
-                descripcionPersonaje
-            );
-        });
+        mostrarPersonajes(data.items);
     })
     .catch((error) => {
         console.error("Ha habido un fallo en la solicitud", error);
@@ -31,13 +15,15 @@ function nuevoPersonaje(
     nombrePersonaje,
     energiaPersonaje,
     razaPersonaje,
+    afiliacionPersonaje,
     generoPersonaje,
     imagenPersonaje,
-    descripcionPersonaje
+    descripcionPersonaje, 
+    id 
 ) {
     const fichaPersonaje = document.createElement("div");
     fichaPersonaje.setAttribute("class", "personajeDB");
-
+    fichaPersonaje.addEventListener ("click",()=>informacionadicional(id))
     const FotoPersonaje = document.createElement("img");
     FotoPersonaje.src = imagenPersonaje;
     const NombredelPersonaje = document.createElement("p");
@@ -46,6 +32,8 @@ function nuevoPersonaje(
     EnergiadelPersonaje.innerText = energiaPersonaje;
     const RazadelPersonaje = document.createElement("p");
     RazadelPersonaje.innerText = razaPersonaje;
+    const AfiliaciondelPersonaje = document.createElement("p");
+    AfiliaciondelPersonaje.innerText = afiliacionPersonaje;
     const GenerodelPersonaje = document.createElement("p");
     GenerodelPersonaje.innerText = generoPersonaje;
     const DescripciondelPersonaje = document.createElement("p");
@@ -56,6 +44,7 @@ function nuevoPersonaje(
         NombredelPersonaje,
         EnergiadelPersonaje,
         RazadelPersonaje,
+        AfiliaciondelPersonaje,
         GenerodelPersonaje,
         DescripciondelPersonaje
     );
@@ -81,24 +70,7 @@ function buscarNombrePersonaje() {
             } else {
                 personajes = data.items;
             }
-            listaPersonajes.innerHTML = "";
-            personajes.forEach((ch) => {
-                const nombrePersonaje = ch.name;
-                const energiaPersonaje = ch.ki;
-                const razaPersonaje = ch.race;
-                const generoPersonaje = ch.gender;
-                const imagenPersonaje = ch.image;
-                const descripcionPersonaje = ch.description;
-
-                nuevoPersonaje(
-                    nombrePersonaje,
-                    energiaPersonaje,
-                    razaPersonaje,
-                    generoPersonaje,
-                    imagenPersonaje,
-                    descripcionPersonaje
-                );
-            });
+            mostrarPersonajes(personajes);
         })
         .catch((error) => {
             console.error("Ha habido un fallo en la solicitud", error);
@@ -112,7 +84,7 @@ function paginaAnterior() {
     fetch(botonpaginaAnt.getAttribute("href"))
         .then((response) => response.json())
         .then((data) => {
-            if (data.meta.currentPage ==1) {
+            if (data.meta.currentPage == 1) {
                 botonpaginaAnt.setAttribute("disabled", "true");
             }
             botonpaginaAnt.removeAttribute("disabled");
@@ -122,23 +94,7 @@ function paginaAnterior() {
             listaPersonajes.innerHTML = "";
             pagAnt = data.links.previous;
             pagSig = data.links.next;
-            data.items.forEach((ch) => {
-                const nombrePersonaje = ch.name;
-                const energiaPersonaje = ch.ki;
-                const razaPersonaje = ch.race;
-                const generoPersonaje = ch.gender;
-                const imagenPersonaje = ch.image;
-                const descripcionPersonaje = ch.description;
-
-                nuevoPersonaje(
-                    nombrePersonaje,
-                    energiaPersonaje,
-                    razaPersonaje,
-                    generoPersonaje,
-                    imagenPersonaje,
-                    descripcionPersonaje
-                );
-            });
+            mostrarPersonajes(data.items);
         });
 }
 
@@ -159,66 +115,77 @@ function paginaSiguiente() {
             listaPersonajes.innerHTML = "";
             pagAnt = data.links.previous;
             pagSig = data.links.next;
-            data.items.forEach((ch) => {
-                const nombrePersonaje = ch.name;
-                const energiaPersonaje = ch.ki;
-                const razaPersonaje = ch.race;
-                const generoPersonaje = ch.gender;
-                const imagenPersonaje = ch.image;
-                const descripcionPersonaje = ch.description;
-
-                nuevoPersonaje(
-                    nombrePersonaje,
-                    energiaPersonaje,
-                    razaPersonaje,
-                    generoPersonaje,
-                    imagenPersonaje,
-                    descripcionPersonaje
-                );
-            });
+            mostrarPersonajes(data.items);
         });
 }
-const eleccionGenero = document.getElementById ("selecciongenero");
-const eleccionRaza = document.getElementById ("seleccionraza");
-const botonFiltros = document.getElementById ("filtrosbtn");
-botonFiltros.addEventListener ("click", personajesseleccionados)
-function personajesseleccionados () {
-        const razaescogida = encodeURIComponent(eleccionRaza.value);
-    
-        fetch("https://dragonball-api.com/api/characters?gender="+eleccionGenero.value+"&race="+razaescogida)
+const eleccionGenero = document.getElementById("selecciongenero");
+const eleccionRaza = document.getElementById("seleccionraza");
+const eleccionAfiliacion = document.getElementById("seleccionafiliacion");
+const botonFiltros = document.getElementById("filtrosbtn");
+botonFiltros.addEventListener("click", personajesseleccionados);
+
+function personajesseleccionados() {
+    const razaescogida = encodeURIComponent(eleccionRaza.value);
+    const afiliacionescogida = encodeURIComponent(eleccionAfiliacion.value);
+
+    fetch(
+        "https://dragonball-api.com/api/characters?gender=" +
+            eleccionGenero.value +
+            "&race=" +
+            razaescogida +
+            "&affiliation=" +
+            afiliacionescogida
+    )
         .then((response) => response.json())
         .then((data) => {
-            botonpaginaAnt.setAttribute ("disabled", "true")
-            botonpaginaSig.setAttribute ("disabled", "true")
+            botonpaginaAnt.setAttribute("disabled", "true");
+            botonpaginaSig.setAttribute("disabled", "true");
 
             let personajes = [];
             if (eleccionGenero) {
-
                 personajes = data;
             } else {
                 personajes = data.items;
             }
-            listaPersonajes.innerHTML = "";
-            personajes.forEach((ch) => {
-                const nombrePersonaje = ch.name;
-                const energiaPersonaje = ch.ki;
-                const razaPersonaje = ch.race;
-                const generoPersonaje = ch.gender;
-                const imagenPersonaje = ch.image;
-                const descripcionPersonaje = ch.description;
-
-                nuevoPersonaje(
-                    nombrePersonaje,
-                    energiaPersonaje,
-                    razaPersonaje,
-                    generoPersonaje,
-                    imagenPersonaje,
-                    descripcionPersonaje
-                );
-            });
+            mostrarPersonajes(personajes);
         })
         .catch((error) => {
             console.error("Ha habido un fallo en la solicitud", error);
         });
+}
+function mostrarPersonajes(personajes) {
+    listaPersonajes.innerHTML = "";
+    personajes.forEach((ch) => {
+        const nombrePersonaje = ch.name;
+        const energiaPersonaje = ch.ki;
+        const razaPersonaje = ch.race;
+        const afiliacionpersonaje = ch.affiliation;
+        const generoPersonaje = ch.gender;
+        const imagenPersonaje = ch.image;
+        const descripcionPersonaje = ch.description;
 
+        nuevoPersonaje(
+            nombrePersonaje,
+            energiaPersonaje,
+            razaPersonaje,
+            afiliacionpersonaje,
+            generoPersonaje,
+            imagenPersonaje,
+            descripcionPersonaje, 
+            ch.id 
+        );
+    });
+}
+
+function informacionadicional(id) {
+    fetch("https://dragonball-api.com/api/characters/" + id)
+        .then((response) => response.json())
+        .then((data) => {
+            const masinfo = document.getElementById("masinfopersonaje"); 
+            const {name, description, image} = data.originPlanet;
+            console.log (data)
+           masinfo.show()
+            masinfo.innerHTML = "<p><b>Personaje: </b>"+data.name.toUpperCase()+"</p><b>Planeta de origen: </b>"+name+"</p><p><b>Descripción: </b>"+description+"</p><p><img src='"+image+"'/></p><button onclick='document.getElementById(`masinfopersonaje`).close()'>Cerrar</button>";
+        })
+        .catch(error => console.log("Ha habido un fallo en la solicitud", error));
     }
